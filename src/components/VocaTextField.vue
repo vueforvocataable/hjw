52380699894a015c2d095a3ce8d62d8d24f1ff46
JungHoon Park <wfr112@naver.com>
2018-09-10 21:42:17 +0900

    added excel file uploading

--- src/components/VocaTextField.vue
+++ src/components/VocaTextField.vue
@@ -1,101 +1,153 @@
 <template>
   <div>
-    <b-form-textarea autofocus
-      id="inputField"
-      v-model="text"
-      rows="10"
-      placeholder="test, 테스트
-  set, 설정">
-</b-form-textarea>
+    <!-- TODO: 레이아웃 설정 -->
+    <b-form-textarea autofocus id="inputField" v-model="text" rows="10" class="w-50">
+    </b-form-textarea>
     <br>
-    <b-button variant="primary" @click="sendVocaToTable">변환</b-button>
-    <b-button variant="primary" @click="downloadVoca">파일 저장</b-button>
+    <b-button variant="primary" v-on:click="sendVocaToTable()">ConvertToVoca</b-button>
+    <b-button variant="primary" v-on:click="downloadVoca()">SaveFile</b-button>
+
+    <div>
+      <label for="excelFileInput" class="btn">ConvertExcelToVoca</label>
+      <input id="excelFileInput" type="file" ref="excelFileInput" multiple v-on:change="excelToVoca()" accept=".xlsx" />
+    </div>
   </div>
 </template>
 
 <script>
-import { saveAs } from '@elastic/filesaver'
+  import {
+    saveAs
+  } from '@elastic/filesaver';
+  import XLSX from 'xlsx';
 
-export default {
-  name: 'VocaTextField',
-  data () {
-    return {
-      //텍스트 에이리어에 있는 텍스트를 담는 변수
-      text: "",
-      //텍스트를 리폼한 단어를 담는 변수
-      voca: []
-    }
-  },
-  created () {
-    this.getSavedDataOnLocalStorage()
-  },
-  destroyed () {
-    this.saveDataOnLocalStorage();
-  },
-  methods: {
-    //입력받은 텍스트를 다듬은 후, 문자열 배열로 바꿔줌
-    reformText: function(text) {
-      text = text.replace(/\n/g, ",").split(',') //엔터값없애줌
-      .map((item) => { //공백없애줌
-        return item.trim()
-      })
-      .filter((item) => { //""값 없애줌
-        return item != ""
-      })
-
-      return text
+  export default {
+    name: 'VocaTextField',
+    data() {
+      return {
+        //텍스트 에이리어에 있는 텍스트를 담는 변수
+        text: "test, 테스트 \nset, 설정",
+        //텍스트를 리폼한 단어를 담는 변수
+        voca: []
+      }
     },
-    //유저가 작성한 텍스트를 테이블에 보낼 형식으로 바꿔주는 함수
-    formatTextToVoca: function(text) {
-      var vocaObj = new Array()
-      var cnt = 0
-      var englishItemTemp
+    created() {
+      this.getSavedDataOnLocalStorage();
+    },
+    destroyed() {
+      this.saveDataOnLocalStorage();
+    },
+    methods: {
+      //입력받은 텍스트를 다듬은 후, 문자열 배열로 바꿔줌
+      reformText: function (text) {
+        text = text.replace(/\n/g, ",").split(',') //엔터값없애줌
+          .map((item) => { //공백없애줌
+            return item.trim();
+          })
+          .filter((item) => { //""값 없애줌
+            return item != "";
+          });
 
-      text = text.forEach((item) => {
-        cnt++
-        if (cnt % 2 == 1) {
-          englishItemTemp = item
-        } else {
-          vocaObj.push({
-            "english": englishItemTemp,
-            "korean": item
+        return text;
+      },
+      //유저가 작성한 텍스트를 테이블에 보낼 형식으로 바꿔주는 함수
+      formatTextToVoca: function (text) {
+        let vocaObj = new Array();
+        let cnt = 0;
+        let englishItemTemp;
+
+        text.forEach((item) => {
+          cnt++;
+          if (cnt % 2 == 1) {
+            englishItemTemp = item;
+          } else {
+            vocaObj.push({
+              "english": englishItemTemp,
+              "korean": item
+            });
+          }
+        });
+
+        return vocaObj;
+      },
+      //버튼클릭시 App.vue로 값을 보냄
+      sendVocaToTable: function () {
+        this.text = this.reformText(this.text);
+        this.voca = this.formatTextToVoca(this.text);
+
+        //router에서 table로 값을 전달함
+        this.$router.push({
+          name: 'Table',
+          params: {
+            vocaProp: this.voca
+          }
+        });
+      },
+      //파일 저장 버튼 누르면 실행
+      //텍스트 필드의 텍스트를 파일로 저장함
+      downloadVoca: function () {
+        if (this.text.length < 1) return;
+
+        var blob = new Blob([this.text], {
+          type: "text/plain;charset=utf-8"
+        });
+        saveAs(blob, "Voca.txt");
+      },
+      //로컬스토리지에 저장한 데이터를 가져옴
+      getSavedDataOnLocalStorage: function () {
+        let savedVocas = JSON.parse(localStorage.getItem('savedItems'));
+
+        if (!savedVocas) return;
+
+        savedVocas.forEach((item) => {
+          this.text += `${item.english}, ${item.korean}\n`;
+        })
+      },
+      //값을 전달하기전 로컬스토리지에 저장한다
+      saveDataOnLocalStorage: function () {
+        if (this.voca.length < 1) return;
+        localStorage.setItem('savedItems', JSON.stringify(this.voca));
+      },
+      //엑셀파일을 텍스트필드에 사용할 형태로 바꿈
+      excelToVoca: function () {
+        let uploadedFiles = this.$refs.excelFileInput.files;
+        let f = uploadedFiles[0];
+        let reader = new FileReader();
+        let self = this;
+
+        reader.onload = function (e) {
+          let data = e.target.result;
+          data = new Uint8Array(data);
+          let workbook = XLSX.read(data, {
+            type: "array"
+          });
+
+          /* DO SOMETHING WITH workbook HERE */
+          let first_sheet_name = workbook.SheetNames[0];
+          /* Get worksheet */
+          let worksheet = workbook.Sheets[first_sheet_name];
+          //It will prints with header and contents ex) Name, Home...
+          let json = XLSX.utils.sheet_to_json(worksheet, {
+            header: 1
+          });
+
+          //Json to text
+          let key = 0;
+          let value = 1;
+          json.forEach(x => {
+            self.text += `${x[key]}, ${x[value]}\n`;
           })
         }
-      })
-
-      return vocaObj
-    },
-    //버튼클릭시 App.vue로 값을 보냄
-    sendVocaToTable: function() {
-      this.text = this.reformText(this.text)
-      this.voca = this.formatTextToVoca(this.text)
-
-      //router에서 table로 값을 전달함
-      this.$router.push({ name: 'Table', params: { vocaProp: this.voca }})
-    },
-    //파일 저장 버튼 누르면 실행
-    //텍스트 필드의 텍스트를 파일로 저장함
-    downloadVoca: function() {
-      if (this.text.length < 1) return
-
-      var blob = new Blob([this.text], {type: "text/plain;charset=utf-8"});
-      saveAs(blob, "Voca.txt");
-    },
-    //로컬스토리지에 저장한 데이터를 가져옴
-    getSavedDataOnLocalStorage: function() {
-      let savedvocas = JSON.parse(localStorage.getItem('savedItems'))
-      savedvocas.forEach((item) => {
-          this.text += `${item.english}, ${item.korean}\n`
-      })
-    },
-    //값을 전달하기전 로컬스토리지에 저장한다
-    saveDataOnLocalStorage: function() {
-      if (this.voca.length < 1) return
-      localStorage.setItem('savedItems', JSON.stringify(this.voca))
+        reader.readAsArrayBuffer(f);
+      }
     }
   }
-}
+
 </script>
 
 <style>
+  #excelFileInput {
+    /* display: inline-block; */
+    visibility: hidden;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 import Routes from './routes/index.js'
 
 Vue.use(VueRouter) 
-
+//TODO 라우터 정리 vue router 강의 볼것
 const router = new VueRouter({
   routes: Routes
 })
